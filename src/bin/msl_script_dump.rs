da27a28ed0b530299d679ddffdbdb121389060bd
Misty De Meo <mistydemeo@gmail.com>
2017-07-29 13:32:20 -0700

    v0.2.0

--- src/bin/msl_script_dump.rs
+++ src/bin/msl_script_dump.rs
@@ -61,7 +61,7 @@ fn process_file(input_file : &String) -> io::Result<Vec<Dialogue>> {
 
 fn main() {
     let matches = App::new("msl_script_dump")
-                          .version("0.1.0")
+                          .version("0.2.0")
                           .author("Misty De Meo")
                           .about("Extract Magical School Lunar! script data")
                           .arg(Arg::with_name("input")
